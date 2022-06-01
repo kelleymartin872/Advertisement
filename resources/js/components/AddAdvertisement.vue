20f9f5f441cc1eb6349f94f31118b80524ca4901
H@ppyElephant <kelleymartin872@gmail.com>
2022-06-01 23:40:58 +0500

    validation add advertisement

--- resources/js/components/AddAdvertisement.vue
+++ resources/js/components/AddAdvertisement.vue
@@ -8,42 +8,69 @@
         >
             {{ status_msg }}
         </div>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Title</label>
-                    <input type="text" class="form-control" v-model="advertisement.title">
-                </div><br>
-                <div class="form-group">
-                    <label>Description</label>
-                    <textarea v-model="advertisement.description" class="form-control"></textarea>
-                </div><br>
-                <div class="form-group">
-                    <label>Files</label>
-                    <input class="form-control" type="file" id="files" ref="files" multiple @change="handleFileUploads()"/>
-                </div>
-                <div class="large-12 medium-12 small-12 cell">
-                    <div v-for="(file, key) in files" class="file-listing">{{ file.name }}
-                        <span class="remove-file" @click="removeFile(key)">Remove</span>
+        <form @submit.prevent="addAdvertisement">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Title</label>
+                        <input type="text" class="form-control" :class="{'is-invalid': v$.advertisement.title.$errors.length}" v-model="advertisement.title">
+                        <div v-for="(error, index) of v$.advertisement.title.$errors" :key="index">
+                            <div class="error-msg">{{ error.$message }}</div>
+                        </div>
                     </div>
-                </div><br>
-                <div class="large-12 medium-12 small-12 cell">
-                    <button @click="addFiles()" class="btn btn-outline-success">Add Files</button>
-                </div><br><br>
-                <button type="submit" class="btn btn-primary" @click="addAdvertisement()">Add Advertisement</button>
+                    <br>
+                    <div class="form-group">
+                        <label>Description</label>
+                        <textarea v-model="advertisement.description" :class="{'is-invalid': v$.advertisement.description.$errors.length}" class="form-control"></textarea>
+                        <div v-for="(error, index) of v$.advertisement.description.$errors" :key="index">
+                            <div class="error-msg">{{ error.$message }}</div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label>Files</label>
+                        <input class="form-control" type="file" id="files" ref="files" multiple @change="handleFileUploads()"/>
+                    </div>
+                    <div class="large-12 medium-12 small-12 cell">
+                        <div v-for="(file, key) in files" class="file-listing">{{ file.name }}
+                            <span class="remove-file" @click="removeFile(key)">Remove</span>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="large-12 medium-12 small-12 cell">
+                        <button @click.prevent="addFiles" class="btn btn-outline-success">Add Files</button>
+                    </div>
+                    <br><br>
+                    <button class="btn btn-primary">Add Advertisement</button>
+                </div>
             </div>
-        </div>
+        </form>
     </div>
 </template>
 
 <script>
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
 export default {
+    setup: () => ({v$: useVuelidate()}),
     data() {
         return {
-            advertisement: {},
+            advertisement: {
+                title: null,
+                description: null,
+            },
             files: [],
             status_msg: '',
-            maxImages: 3
+            maxImages: 3,
+        }
+    },
+    validations() {
+        return {
+            advertisement: {
+                title: { required },
+                description: { required },
+            },
         }
     },
     methods: {
@@ -72,9 +99,12 @@ export default {
                 this.files.push(...uploadedFiles)
             }
         },
-        addAdvertisement() {
+        async addAdvertisement() {
+            const isFormCorrect = await this.v$.$validate()
+            if (!isFormCorrect) return
+
             let formData = new FormData();
-            for( var i = 0; i < this.files.length; i++ ){
+            for (var i = 0; i < this.files.length; i++) {
                 let file = this.files[i];
                 formData.append('files[' + i + ']', file);
             }
@@ -87,20 +117,18 @@ export default {
                 }
             }
 
-            this.$axios.get('/sanctum/csrf-cookie').then(response => {
-                this.$axios.post('/api/advertisement/add', formData, config)
-                    .then(response => {
-                        this.$router.push({name: 'home'})
-                    })
-                    .catch(function (error) {
-                        console.error(error);
-                    });
-            })
+            try {
+                await this.$axios.get('/sanctum/csrf-cookie')
+                await this.$axios.post('/api/advertisement/add', formData, config)
+                await this.$router.push({name: 'home'})
+            } catch (e) {
+                console.error(e);
+            }
         },
-        removeFile( key ) {
+        removeFile(key) {
             this.files.splice(key, 1);
         },
-        showNotification (message) {
+        showNotification(message) {
             this.status_msg = message
             setTimeout(() => {
                 this.status_msg = ''
@@ -127,5 +155,8 @@ span.remove-file {
     color: red;
     cursor: pointer;
     float: right;
+}
+.error-msg {
+    color: #dc3545
 }
 </style>
